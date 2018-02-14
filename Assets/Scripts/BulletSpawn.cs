84be881e69fa6edd9e160fb3a8bb07437456de65
student <student@ssu-cs-dar25-167-149.sonoma.edu>
2018-02-13 20:32:56 -0800

    added bullet spawner.

---
+++ Assets/Scripts/BulletSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class BulletSpawn : MonoBehaviour {
+	public float lives;
+	public float spawnTime;
+	public GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn ()
+	{
+		if (lives <= 0)
+			return;
+
+		System.Random rnd = new System.Random ();
+
+		Instantiate (bullet, new Vector3 (rnd.Next (-24, 24), rnd.Next (-24, 24), 25), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/bullet_behavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet_behavior : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float speed;
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * speed * -1;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
